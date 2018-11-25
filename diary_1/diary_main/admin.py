86966d0ab471c90dce513aaf6800bdde137df0e9
lcc1999 <43164675+lcc1999@users.noreply.github.com>
2018-11-25 10:50:07 +0800

    Update admin.py

--- diary_1/diary_main/admin.py
+++ diary_1/diary_main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Diary,User
 
 # Register your models here.
+class DiaryInline(admin.StackedInline):
+    model = Diary
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['username','password','img','birthday','sex','telephone']}),
+    ]
+    inlines = [DiaryInline]
+
+admin.site.register(User, UserAdmin)
