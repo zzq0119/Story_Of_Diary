e17a4aace550fd609d5201b56d008d75ac4e26c3
lcc1999 <43164675+lcc1999@users.noreply.github.com>
2018-12-01 10:10:42 +0800

    Update views.py

--- diary_1/diary_main/views.py
+++ diary_1/diary_main/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404,render,redirect
+from django.http import HttpResponseRedirect,HttpResponseForbidden
 from django.urls import reverse
 from .models import Diary,User
 from datetime import date
 from django.template import loader,RequestContext
-import datetime
+import datetime,re
 
 def index(request):
     if request.method=='POST' and 'check_password' not in request.POST:
@@ -31,7 +31,7 @@ def index(request):
         if _password==check_password:
             user=User(username=user_name,password=_password)
             user.save()
-            return HttpResponseRedirect('signUp',request)
+            return redirect('signUp')
         else:
             return render(request,'index.html',{'error_message':'Inconsistent password.'})
     else:
@@ -43,8 +43,7 @@ def signUp(request):
 def signOut(request):
     if request.session:
         request.session.flush()
-    return render(request,'index.html')
-    
+    return redirect('/')
 def public(request,page):
     if 'u_id' in request.session and 'username' in request.session:
         user_name=request.session['username']
@@ -80,10 +79,18 @@ def private(request,page):
         user_name=request.session['username']
         u_id=request.session['u_id']
         user=get_object_or_404(User,id=u_id)
+        diary_list=[]
         diary_list=user.diary_set.all()
+        if request.method=="POST":
+            tmp=[]
+            diary_list=user.diary_set.all()
+            for i in diary_list:
+                if str(i.pub_date.date())==request.POST.get("date"):
+                    tmp+=[i]
+            diary_list=tmp
         length=len(diary_list)
-        max_page=int((length-1)/6)+1
-        diary_list=diary_list[page*6-6:page*6]
+        max_page=int((length-1)/5)+1
+        diary_list=diary_list[page*5-5:page*5]
         dist={}
         if page>1 and page<max_page:
             dist['last']=reverse('private',args=(page-1,))
@@ -95,8 +102,8 @@ def private(request,page):
         for i in range(len(diary_list)):
             dist['diary'+str(i+1)]=diary_list[i]
             dist['url'+str(i+1)]=reverse('private_detail',args=(diary_list[i].id,))
-        dist['user']=user.realname
         dist['age']=datetime.datetime.today().year-User.objects.get(id=u_id).birthday.year
+        dist['user']=user.realname
         dist['public']=reverse('public',args=(1,))
         dist['private']=reverse('private',args=(1,))
         dist['setting']=reverse('private_setting')
@@ -121,8 +128,8 @@ def private_setting(request):
                     user.sex="女"
                 else:
                     user.sex="男"
-            if request.POST.get("year") and request.POST.get("month") and request.POST.get("day"):
-                user.birthday=date(int(request.POST.get("year")),int(request.POST.get("month")),int(request.POST.get("day")))
+            if request.POST.get("YYYY") and request.POST.get("MM") and request.POST.get("DD"):
+                user.birthday=date(int(request.POST.get("YYYY")),int(request.POST.get("MM")),int(request.POST.get("DD")))
             if request.POST.get("telephone"):
                 user.telephone=request.POST.get("telephone")
             if request.POST.get("mailbox"):
@@ -139,7 +146,7 @@ def public_detail(request,d_id):
         user=User.objects.get(id=u_id)
         diary=Diary.objects.get(id=d_id)
         diary_list=Diary.objects.filter(public=True)
-        dist={'back':reverse('public',args=(1,)),'list':diary_list,'pub_date':diary.pub_date,'realname':user.realname,'age':datetime.datetime.today().year-user.birthday.year,
+        dist={'back':reverse('public',args=(1,)),'picture':user,'list':diary_list,'pub_date':diary.pub_date,'realname':user.realname,'age':datetime.datetime.today().year-user.birthday.year,
               'user_name':user.username,'title':diary.title,'text':diary.diary_text,'author':diary.user.username,'email':user.email}
         if user.sex=="男":
             dist['sex']="男"
@@ -152,13 +159,15 @@ def private_detail(request,d_id):
         user=User.objects.get(id=u_id)
         diary=Diary.objects.get(id=d_id)
         diary_list=user.diary_set.all()
-        dist={'back':reverse('private',args=(1,)),'list':diary_list,'pub_date':diary.pub_date,'realname':user.realname,'age':datetime.datetime.today().year-user.birthday.year,
+        dist={'back':reverse('private',args=(1,)),'piture':user,'list':diary_list,'pub_date':diary.pub_date,'realname':user.realname,'age':datetime.datetime.today().year-user.birthday.year,
               'user_name':user.username,'title':diary.title,'text':diary.diary_text,'url':reverse('private_edit',args=(d_id,)),'email':user.email}
         if user.sex=="男":
             dist['sex']="男"
         if user.sex=="女":
             dist['sex']="女"
         return render(request,'private_detail.html',dist)
+    else:
+        return redirect('/')
 def private_edit(request,d_id):
     diary=get_object_or_404(Diary,id=d_id)
     mess='私有'
@@ -196,3 +205,5 @@ def private_edit_new(request):
             dist={'back':reverse('private',args=(1,)),'title':diary.title,'text':diary.diary_text,'url':reverse('private_edit',args=(diary.id,))}
             return render(request,'private_detail.html',dist)
         return render(request,'private_edit_new.html',{'url':reverse('private',args=(1,))})
+    else:
+        return redirect('/')
