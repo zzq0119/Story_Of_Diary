ebcecc1aed3235633d6e163a25821a8b64fd9b6b
zzq0119 <498328710@qq.com>
2018-12-01 19:23:30 +0800

    auto_commit

--- diary_1/diary_main/views.py
+++ diary_1/diary_main/views.py
@@ -210,7 +210,12 @@ def private_detail(request,d_id):
     if 'u_id' in request.session and 'username' in request.session:
         u_id=request.session['u_id']
         user=User.objects.get(id=u_id)
-        diary=Diary.objects.get(id=d_id)
+        try:
+            diary=Diary.objects.get(id=d_id)
+        except(Diary.DoesNotExist):
+            dist={'back':reverse('private',args=(1,)),'piture':user,'realname':user.realname,'age':datetime.datetime.today().year-user.birthday.year,
+              'user_name':user.username,'url':reverse('private_edit',args=(d_id,)),'email':user.email,'d_id':d_id}
+            return render(request,'private_detail.html',dist)
         if request.method=="POST":
             diary.delete()
             return HttpResponseRedirect(reverse('private',args=(1,)))
@@ -313,7 +318,7 @@ def private_edit_new(request):
             dist['img']=user.img
             dist['setting']=reverse('private_setting')
             return redirect('/private/page/1')
-        dist={'url':reverse('private',args=(1,)),'picture':user,'year':user.birthday.year,'month':user.birthday.month,'day':user.birthday.day,'phone':user.telephone,'email':user.email,'name':user.realname,'age':datetime.datetime.today().year-user.birthday.year,'email':user.email,'public':reverse('public',args=(1,)),'d_id':diary.id,'private':reverse('private',args=(1,))}
+        dist={'url':reverse('private',args=(1,)),'picture':user,'year':user.birthday.year,'month':user.birthday.month,'day':user.birthday.day,'phone':user.telephone,'email':user.email,'name':user.realname,'age':datetime.datetime.today().year-user.birthday.year,'email':user.email,'public':reverse('public',args=(1,)),'private':reverse('private',args=(1,))}
         dist['setting']=reverse('private_setting')
         dist['img']=user.img
         dist['help']=reverse('help')
